388fcf5f967e1f0d0f25cc5ac66bf8da42bbf288
kind-sailor-1643 <bhandarireeva@gmail.com>
2019-06-27 10:33:29 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,7 +1,7 @@
 # code the #greeting method here!
 def greeting(name)
   name=name.strip
-  return puts "Hello #{name}.It's nice to meet you."
+  return puts "Hello #{name}. It's nice to meet you."
 end
 
 
